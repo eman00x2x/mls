dbbe687bf72b6746c29bab43310e9e6d597fc651
eman00x2x <eman00x2xx@gmail.com>
2024-03-05 18:14:41 +0800

    3/5/2024 6:14pm

--- Admin/Application/Controller/DashboardController.php
+++ Admin/Application/Controller/DashboardController.php
@@ -15,16 +15,19 @@ class DashboardController extends \Main\Controller {
 
 	function index() {
 		
+		$this->getTrafficChart();
 		$this->getChartEarningsThisYear();
 
+		$data['most_traffic'] = $this->getMostTraffic();
 		$data['total_accounts'] = $this->getTotalAccounts();
 		$data['total_earnings'] = $this->getEarningsThisWeek();
 
 		$this->setTemplate("dashboard/index.php");
 		return $this->getTemplate($data);
+
 	}
 
-	function getTotalAccounts() {
+	function getTotalAccounts(): int {
 
 		$accounts = $this->getModel("Account");
 		$accounts->select(" COUNT(account_id) as total ");
@@ -35,7 +38,7 @@ class DashboardController extends \Main\Controller {
 
 	}
 
-	 function getEarningsThisWeek() {
+	function getEarningsThisWeek(): int {
 
 		$transactions = $this->getModel("Transaction");
 		$transactions->select(" SUM(premium_price) as total ");
@@ -51,7 +54,57 @@ class DashboardController extends \Main\Controller {
 
 	}
 
-	function getChartEarningsThisYear() {
+	function getMostTraffic($account_id = null): Array {
+
+		$traffic = $this->getModel("ListingView");
+        $traffic->page['limit'] = 100000;
+
+		$traffic->select(" t.listing_id, title, CONCAT(a.firstname, ' ', a.lastname) as posted_by, COUNT(session_id) as count ");
+		$traffic->join(" t JOIN #__listings l ON t.listing_id=l.listing_id JOIN #__accounts a ON a.account_id=t.account_id ");
+
+		if($account_id != null) {
+			$traffic->where(" t.account_id = $account_id ");
+		}
+
+		$traffic->groupBy(" t.listing_id ");
+        $data = $traffic->getList();
+debug($data);
+		return $data;
+
+	}
+
+	function getTrafficChart($account_id = null): void {
+
+		$traffic = $this->getModel("ListingView");
+        $traffic->page['limit'] = 100000;
+
+		$traffic->select(" FROM_UNIXTIME(created_at, '%Y-%m-%d') as date, COUNT(session_id) as count ");
+
+		if($account_id != null) {
+			$traffic->where(" account_id = $account_id ");
+		}
+
+		$traffic->groupBy(" date ");
+
+        $data = $traffic->getList();
+
+		if($data) {
+
+			for($i=0; $i<count($data); $i++) {
+				$chart['series'][] = $data[$i]['count'];
+				$chart['labels'][] = $data[$i]['date'];
+			}
+
+			$chart_data['labels'] = json_encode($chart['labels']);
+			$chart_data['series'] = json_encode($chart['series']);
+
+		}
+
+		$this->getLibrary("Charts")->getLineChart($chart_data, "getTrafficChart", "Traffic");
+
+	}
+
+	function getChartEarningsThisYear(): void {
 
 		$from_date = strtotime(date("Y-01-01", DATE_NOW));
 		$to_date = strtotime(date("Y-12-t", DATE_NOW));
--- Admin/Application/View/dashboard/index.php
+++ Admin/Application/View/dashboard/index.php
@@ -83,88 +83,42 @@ $html[] = "<div class='page-body'>";
 
 			$html[] = "<div class='col-6'>";
 				$html[] = "<div class='card mb-3'>";
+					$html[] = "<div class='card-body'>";
+						$html[] = "<h3 class='card-title'>Property Listings Traffic</h3>";
+						$html[] = "<div id='getTrafficChart' class='chart-lg'></div>";
+					$html[] = "</div>";
+				$html[] = "</div>";
+			$html[] = "</div>";
+
+			$html[] = "<div class='col-6'>";
+				$html[] = "<div class='card mb-3'>";
 					$html[] = "<div class='card-header'>";
 						$html[] = "<h3 class='card-title'>Most Visited Pages</h3>";
 					$html[] = "</div>";
+
+					$html[] = "<div class='card-table table-responsive'>";
+						$html[] = "<table class='table table-vcenter'>";
+						$html[] = "<thead>";
+							$html[] = "<th>Posting Title</th>";
+							$html[] = "<th class='text-center'>Visitors</th>";
+							$html[] = "<th class='text-center'>Posted By</th>";
+							$html[] = "<th></th>";
+						$html[] = "</thead>";
+
+						if($data['most_traffic']) {
+							for($i=0; $i<count($data['most_traffic']); $i++) {
+								$html[] = "<tr>";
+									$html[] = "<td>".$data['most_traffic'][$i]['title']."</td>";
+									$html[] = "<td class='text-center'>".$data['most_traffic'][$i]['count']."</td>";
+									$html[] = "<td class='text-center'>".$data['most_traffic'][$i]['posted_by']."</td>";
+									$html[] = "<td></td>";
+								$html[] = "</tr>";
+							}
+						}
+
+						$html[] = "</table>";
+					$html[] = "</div>";
 					
-					$html[] = '<div class="card-table table-responsive">
-						<table class="table table-vcenter">
-							<thead>
-								<tr>
-									<th>Post name</th>
-									<th>Visitors</th>
-									<th>Unique</th>
-									<th colspan="2">Bounce rate</th>
-								</tr>
-							</thead>
-							<tr>
-								<td>
-									Modern 2 storey 5 bedrooms Alabang 400 Village, Muntinlupa City
-									<a href="#" class="ms-1" aria-label="Open website"><!-- Download SVG icon from http://tabler-icons.io/i/link -->
-										<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 15l6 -6" /><path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" /><path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" /></svg>
-									</a>
-								</td>
-								<td class="text-secondary">4,896</td>
-								<td class="text-secondary">3,654</td>
-								<td class="text-secondary">82.54%</td>
-							</tr>
-							<tr>
-								<td>
-									Modern 2 storey 5 bedrooms Alabang 400 Village, Muntinlupa City
-									<a href="#" class="ms-1" aria-label="Open website"><!-- Download SVG icon from http://tabler-icons.io/i/link -->
-										<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 15l6 -6" /><path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" /><path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" /></svg>
-									</a>
-								</td>
-								<td class="text-secondary">3,652</td>
-								<td class="text-secondary">3,215</td>
-								<td class="text-secondary">76.29%</td>
-							</tr>
-							<tr>
-								<td>
-									Modern 2 storey 5 bedrooms Alabang 400 Village, Muntinlupa City
-									<a href="#" class="ms-1" aria-label="Open website"><!-- Download SVG icon from http://tabler-icons.io/i/link -->
-										<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 15l6 -6" /><path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" /><path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" /></svg>
-									</a>
-								</td>
-								<td class="text-secondary">3,256</td>
-								<td class="text-secondary">2,865</td>
-								<td class="text-secondary">72.65%</td>
-							</tr>
-							<tr>
-								<td>
-									Modern 2 storey 5 bedrooms Alabang 400 Village, Muntinlupa City
-									<a href="#" class="ms-1" aria-label="Open website"><!-- Download SVG icon from http://tabler-icons.io/i/link -->
-										<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 15l6 -6" /><path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" /><path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" /></svg>
-									</a>
-								</td>
-								<td class="text-secondary">986</td>
-								<td class="text-secondary">865</td>
-								<td class="text-secondary">44.89%</td>
-							</tr>
-							<tr>
-								<td>
-									Modern 2 storey 5 bedrooms Alabang 400 Village, Muntinlupa City
-									<a href="#" class="ms-1" aria-label="Open website"><!-- Download SVG icon from http://tabler-icons.io/i/link -->
-										<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 15l6 -6" /><path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" /><path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" /></svg>
-									</a>
-								</td>
-								<td class="text-secondary">912</td>
-								<td class="text-secondary">822</td>
-								<td class="text-secondary">41.12%</td>
-							</tr>
-							<tr>
-								<td>
-									Modern 2 storey 5 bedrooms Alabang 400 Village, Muntinlupa City
-									<a href="#" class="ms-1" aria-label="Open website"><!-- Download SVG icon from http://tabler-icons.io/i/link -->
-										<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 15l6 -6" /><path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" /><path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" /></svg>
-									</a>
-								</td>
-								<td class="text-secondary">855</td>
-								<td class="text-secondary">798</td>
-								<td class="text-secondary">32.65%</td>
-							</tr>
-						</table>
-					</div>';
 				$html[] = "</div>";
 			$html[] = "</div>";
 
