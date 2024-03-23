88d2af1bc603ef9ea2093861963658458d3a5d77
eman00x2x <eman00x2xx@gmail.com>
2024-03-23 15:10:52 +0800

    3/23/2024 3:10pm

--- Admin/Application/View/listings/download.php
+++ Admin/Application/View/listings/download.php
@@ -1,6 +1,7 @@
 <?php
 
-$html[] = "<style type=\"text/css\">
+$html[] = "
+<style type=\"text/css\">
 .images {
 	background-color: #999;
 	background-position: center;
@@ -51,7 +52,7 @@ $html[] = "<page style=''>";
 		$html[] = "<h1 style='margin:0; padding:0; font-size:18px;'>[Id: ".$data['listing']['listing_id']."] ".$data['listing']['title']."</h1>";
 		$html[] = "<p style='margin:0; padding:0;'><span><img src='".CDN."images/icons/map-pin.png' style='width:24px;' /> ".$data['listing']['address']['municipality'].", ".$data['listing']['address']['province']."</span></p>";
         
-        $html[] = "<div style='margin:15px 0 0; padding:0 15px;'>";
+        $html[] = "<div style='margin:15px 0 0; padding:5px 15px;'>";
 	
 			$html[] = "<table>";
 			$html[] = "<tr>";
@@ -87,74 +88,125 @@ $html[] = "<page style=''>";
 
         $html[] = "</div>";
 
-		$html[] = "<div style='margin-bottom: 15px; font-size:14px;  padding:25px 15px;'>";
-
-			$html[] = "<table style='width:700px;'>";
-			$html[] = "<tr>";
-				$html[] = "<td colspan='2' style='border-bottom:1px solid #e1e1e1; font-weight:bold;'>".$data['listing']['category']."</td>";
-			$html[] = "</tr>";
-			if($data['listing']['floor_area'] > 0) {
-				$html[] = "<tr>";
-					$html[] = "<td style='border-bottom:1px solid #e1e1e1; text-align: left; '>Floor Area</td>";
-					$html[] = "<td style='border-bottom:1px solid #e1e1e1;'><img src='".CDN."images/icons/ruler.png' style='width:18px;' /> ".number_format($data['listing']['floor_area'],0)." sqm</td>";
-				$html[] = "</tr>";
-			}
+		$html[] = "<div style='margin-bottom: 15px; font-size:14px; padding:5px 15px;'>";
+			$html[] = "<div style='font-size:14px; width:700px;'>";
 			
-			if($data['listing']['lot_area'] > 0) {
+				$html[] = "<h3 style='margin:20px 0 0 0; padding:0;'>Technical Details</h3>";
+				$html[] = "<table style='margin:0 !important; padding:0 !important;'>";
 				$html[] = "<tr>";
-					$html[] = "<td style='border-bottom:1px solid #e1e1e1; text-align: left; '>Lot Area</td>";
-					$html[] = "<td style='border-bottom:1px solid #e1e1e1;'><img src='".CDN."images/icons/ruler.png' style='width:18px;' /> ".number_format($data['listing']['lot_area'],0)." sqm</td>";
+						
+					$html[] = "<td style='padding:10px; border: 1px solid #e1e1e1; width:195px;'>";
+						$html[] = "<div style='color:#555; margin-bottom:5px; font-size:12px;'>Category</div>";
+						$html[] = $data['listing']['category'];
+					$html[] = "</td>";
+
+					$html[] = "<td style='padding:10px; border: 1px solid #e1e1e1; width:195px;'>";
+						$html[] = "<div style='color:#555; margin-bottom:5px; font-size:12px;'>Lot Area</div>";
+						if($data['listing']['floor_area'] > 0) {
+							$html[] = "<img src='".CDN."images/icons/maximize.png' style='width:18px;' /> ".number_format($data['listing']['lot_area'],0)." sqm";
+						}else { $html[] = "<span style='color:#555;'>N/A</span>"; }
+					$html[] = "</td>";
+					
+					$html[] = "<td style='padding:10px; border: 1px solid #e1e1e1; width:195px;'>";
+						$html[] = "<div style='color:#555; margin-bottom:5px; font-size:12px;'>Floor Area</div>";
+						if($data['listing']['floor_area'] > 0) {
+							$html[] = "<img src='".CDN."images/icons/ruler.png' style='width:18px;' /> ".number_format($data['listing']['floor_area'],0)." sqm";
+						}else { $html[] = "<span style='color:#555;'>N/A</span>"; }
+					$html[] = "</td>";
+
 				$html[] = "</tr>";
-			}
-			
-			if($data['listing']['unit_area'] > 0) {
+				
 				$html[] = "<tr>";
-					$html[] = "<td style='border-bottom:1px solid #e1e1e1; text-align: left; '>Unit Area</td>";
-					$html[] = "<td style='border-bottom:1px solid #e1e1e1;'><img src='".CDN."images/icons/ruler-measure.png' style='width:18px;' /> ".number_format($data['listing']['unit_area'],0)." sqm</td>";
-				$html[] = "</tr>";
-			}
 
-			if($data['listing']['bedroom'] != "") {
+					$html[] = "<td style='padding:10px; border: 1px solid #e1e1e1; width:195px;'>";
+						$html[] = "<div style='color:#555; margin-bottom:5px; font-size:12px;'>Bedroom</div>";
+						if($data['listing']['bedroom'] > 0 && $data['listing']['bedroom'] != 'Studio') {
+							$html[] = "<img src='".CDN."images/icons/bed.png' style='width:18px;' /> ".$data['listing']['bedroom']."";
+						}else { $html[] = "<span style='color:#555;'>N/A</span>"; }
+					$html[] = "</td>";
+
+					$html[] = "<td style='padding:10px; border: 1px solid #e1e1e1; width:195px;'>";
+						$html[] = "<div style='color:#555; margin-bottom:5px; font-size:12px;'>Bathroom</div>";
+						if($data['listing']['bathroom'] > 0) {
+							$html[] = "<img src='".CDN."images/icons/bath.png' style='width:18px;' /> ".$data['listing']['bathroom']."";
+						}else { $html[] = "<span style='color:#555;'>N/A</span>"; }
+					$html[] = "</td>";
+
+					$html[] = "<td style='padding:10px; border: 1px solid #e1e1e1; width:195px;'>";
+						$html[] = "<div style='color:#555; margin-bottom:5px; font-size:12px;'>Car Garage</div>";
+						if($data['listing']['parking'] > 0) {
+							$html[] = "<img src='".CDN."images/icons/car-garage.png' style='width:18px;' /> ".$data['listing']['parking']." Car slot";
+						}else { $html[] = "<span style='color:#555;'>No Parkking Slot</span>"; }
+					$html[] = "</td>";
+
+				$html[] = "</tr>";
+				$html[] = "</table>";
+
+				/** PAYMENT DETAILS */
+				$html[] = "<h3 style='margin:20px 0 0 0; padding:0;'>Payment Details</h3>";
+				$html[] = "<table style=''>";
 				$html[] = "<tr>";
-					$html[] = "<td style='border-bottom:1px solid #e1e1e1; text-align: left; '>Bedroom</td>";
-					$html[] = "<td style='border-bottom:1px solid #e1e1e1;'><img src='".CDN."images/icons/bed.png' style='width:18px;' /> ".$data['listing']['bedroom']."</td>";
-				$html[] = "</tr>";
-			}
 
-			if($data['listing']['bathroom'] != "") {
+					$html[] = "<td style='padding:10px; border: 1px solid #e1e1e1; width:315px;'>";
+						$html[] = "<div style='color:#555; margin-bottom:5px; font-size:12px;'>Selling Price</div>";
+						$html[] = number_format($data['listing']['price'],0);
+					$html[] = "</td>";
+
+					$html[] = "<td style='padding:10px; border: 1px solid #e1e1e1; width:315px;'>";
+						$html[] = "<div style='color:#555; margin-bottom:5px; font-size:12px;'>Reservation / Option Money</div>";
+						$html[] = number_format($data['listing']['reservation'],0);
+					$html[] = "</td>";
+
+				$html[] = "</tr>";
 				$html[] = "<tr>";
-					$html[] = "<td style='border-bottom:1px solid #e1e1e1; text-align: left; '>Bathroom</td>";
-					$html[] = "<td style='border-bottom:1px solid #e1e1e1;'><img src='".CDN."images/icons/bath.png' style='width:18px;' /> ".$data['listing']['bathroom']."</td>";
+
+					$html[] = "<td style='padding:10px; border: 1px solid #e1e1e1; width:315px;'>";
+						$html[] = "<div style='color:#555; margin-bottom:5px; font-size:12px;'>Eligible for Bank Loan?</div>";
+						$html[] = ((isset($data['listing']['payment_details']['bank_loan']) ? $data['listing']['payment_details']['bank_loan'] : 0) == 1 ? "Yes" : "No");
+					$html[] = "</td>";
+
+					$html[] = "<td style='padding:10px; border: 1px solid #e1e1e1; width:315px;'>";
+						$html[] = "<div style='color:#555; margin-bottom:5px; font-size:12px;'>Eligible for Pag-Ibig Housing Loan?</div>";
+						$html[] = ((isset($data['listing']['payment_details']['pagibig_loan']) ? $data['listing']['payment_details']['pagibig_loan'] : 0) == 1 ? "Yes" : "No");
+					$html[] = "</td>";
+
 				$html[] = "</tr>";
-			}
 
-			$html[] = "<tr>";
-				$html[] = "<td style='border-bottom:1px solid #e1e1e1; text-align: left; '>Car Garage</td>";
-				$html[] = "<td style='border-bottom:1px solid #e1e1e1;'><img src='".CDN."images/icons/car-garage.png' style='width:18px;' /> ".($data['listing']['parking'] > 0 ? $data['listing']['parking'] : "No Parking")."</td>";
-			$html[] = "</tr>";
-			$html[] = "</table>";
 
+				$html[] = "<tr>";
+					$html[] = "<td style='padding:10px; border: 1px solid #e1e1e1; width:315px;'>";
+						$html[] = "<div style='color:#555; margin-bottom:5px; font-size:12px;'>Assume Balance?</div>";
+						$html[] = ((isset($data['listing']['payment_details']['assume_balance']) ? $data['listing']['payment_details']['assume_balance'] : 0) == 1 ? "Yes" : "No");
+					$html[] = "</td>";
+
+					$html[] = "<td style='padding:10px; border: 1px solid #e1e1e1; width:315px;'></td>";
+				$html[] = "</tr>";
+				$html[] = "</table>";
+			/** PAYMENT DETAILS END */
 
 			/** AMENITIES */
-			$html[] = "<div style='font-size:14px; width:700px;'>";
-				$html[] = "<h3 style='margin:0; padding:0;'>Amenities</h3>";
-				
-					$amenities = explode(",",$data['listing']['amenities']);
-					foreach($amenities as $amenity) {
-						$html[] = "<span>";
-							$html[] = " <img src='".CDN."images/icons/checks.png' style='width:18px;' /> $amenity, ";
-						$html[] = "</span>";
-					}
-						
-			$html[] = "</div>";
-			/** AMENITIES END */
+				$html[] = "<h3 style='margin:20px 0 0 0; padding:0;'>Amenities</h3>";
+				$amenities = explode(",",$data['listing']['amenities']);
+				foreach($amenities as $amenity) {
+					$html[] = "<span>";
+						$html[] = " <img src='".CDN."images/icons/checks.png' style='width:18px;' /> $amenity, ";
+					$html[] = "</span>";
+				}
+				/** AMENITIES END */
 
+			$html[] = "</div>";
 		$html[] = "</div>";
 
-		$html[] = "<div class='description' style=''>";
-        	$html[] = "<h3 style='margin:0 0 10px; padding:0;'>Description</h3>";
-            $html[] = $data['listing']['long_desc'];
-        $html[] = "</div>";
+		$html[] = "<div style='margin-bottom: 15px; font-size:14px; padding:5px 15px;'>";
+			$html[] = "<div style='font-size:14px; width:700px;'>";
+
+				$html[] = "<div style=''>";
+					$html[] = "<h3 style='margin:20px 0 0 0; padding:0;'>Description</h3>";
+					$html[] = $data['listing']['long_desc'];
+				$html[] = "</div>";
+
+			$html[] = "</div>";
+		$html[] = "</div>";
 
     $html[] = "</div>";
 $html[] = "</page>";
