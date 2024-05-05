f515e2612b520fe9786a210f918912cc2cbc1e7e
Eman Olivas <eman00x2xx@gmail.com>
2024-05-05 22:35:30 +0800

    5-5-2024 10:35pm

--- Admin/Application/Controller/ListingsController.php
+++ Admin/Application/Controller/ListingsController.php
@@ -431,7 +431,7 @@ class ListingsController extends \Main\Controller {
 
 						$('.listings-table .avatar').each(function() {
 							thumb_image = $(this).attr('data-thumb-image');
-							$(this).css('background-image', 'url(".CDN."images/loader.gif)');
+							$(this).css('background-image', 'url(".CDN."images/item_default.jpg)');
 							getImage(thumb_image, $(this));
 						});
 					});
--- Website/Application/Controller/ListingsController.php
+++ Website/Application/Controller/ListingsController.php
@@ -357,7 +357,7 @@ class ListingsController extends \Admin\Application\Controller\ListingsControlle
 
 						$('.listings-table .avatar').each(function() {
 							thumb_image = $(this).attr('data-thumb-image');
-							$(this).css('background-image', 'url(".CDN."images/loader.gif)');
+							$(this).css('background-image', 'url(".CDN."images/item_default.jpg)');
 							getImage(thumb_image, $(this));
 						});
 
